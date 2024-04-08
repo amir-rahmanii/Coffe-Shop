8b67a91af965d94390aced2dec82b784a5dbd274
Amirreza Rahmani <amirbabble@gmail.com>
2024-04-09 00:55:58 +0330

    Start CoffeShop

---
+++ Public/scripts/app.js
@@ -0,0 +1,93 @@
+let chengeTheme = document.querySelectorAll('.chengeTheme')
+let componentScroll = document.querySelector('.componentScroll')
+let flag = true;
+let addMenuMobile = document.querySelector('#addMenuMobile')
+let menuMobile = document.querySelector('.menuMobile')
+let closeMenu = document.querySelectorAll('.closeMenu')
+let openSubMenuMobile = document.querySelector('#openSubMenuMobile')
+let openingSubMobile = document.querySelector('#openingSubMobile')
+let chevron = document.querySelector('#chevron')
+let overlay = document.querySelector('.overlay')
+let addShoppingCart = document.querySelector('#addShoppingCart')
+let shoppingCart = document.querySelector('.shoppingCart')
+let flagBySubMenu = true;
+
+window.addEventListener('load', () => {
+    let theme = localStorage.getItem('theme')
+    document.documentElement.classList.add(theme)
+    if (theme === "dark") {
+        flag = false
+    }
+    getComponent()
+})
+
+//desktop
+chengeTheme.forEach((theme) => {
+    theme.addEventListener('click', () => {
+        if (flag) {
+            localStorage.setItem('theme', "dark")
+            document.documentElement.classList.add('dark')
+            flag = false;
+        } else {
+            localStorage.setItem('theme', "light")
+            document.documentElement.classList.remove('dark')
+            flag = true;
+        }
+    })
+})
+
+const getComponent = () => {
+    if (componentScroll.offsetHeight > 342) {
+        componentScroll.style.maxHeight = '342px'
+        componentScroll.style.overflowY = "scroll"
+        componentScroll.style.paddingLeft = "3px"
+        console.log("dsfdsf");
+    }
+}
+
+
+// menu Mobile
+addMenuMobile.addEventListener('click', () => {
+    menuMobile.classList.remove('-right-64')
+    menuMobile.classList.add('right-0')
+    overlay.classList.add('overlay--visible')
+})
+
+const hideMenu = () => {
+    menuMobile.classList.remove('right-0')
+    shoppingCart.classList.remove('left-0')
+    shoppingCart.classList.add('-left-64')
+    menuMobile.classList.add('-right-64')
+    overlay.classList.remove('overlay--visible')
+}
+
+closeMenu.forEach(close => {
+    close.addEventListener('click', () => {
+        hideMenu()
+    })
+})
+overlay.addEventListener('click', () => {
+    hideMenu()
+})
+
+openingSubMobile.addEventListener('click', () => {
+    if (flagBySubMenu) {
+        chevron.innerHTML = `<use href="#chevron-up"></use>`
+        openingSubMobile.classList.add('text-orange-300')
+        openSubMenuMobile.style.display = "flex"
+        flagBySubMenu = false;
+    } else {
+        chevron.innerHTML = `<use href="#chevron-down"></use>`
+        openingSubMobile.classList.remove('text-orange-300')
+        openSubMenuMobile.style.display = "none"
+        flagBySubMenu = true;
+    }
+})
+
+//shoping mobile
+
+addShoppingCart.addEventListener('click', () => {
+    shoppingCart.classList.remove('-left-64')
+    shoppingCart.classList.add('left-0')
+    overlay.classList.add('overlay--visible')
+})
---
+++ tailwind.config.js
@@ -0,0 +1,44 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  darkMode : "class",
+  theme: {
+    extend: {
+      colors : {
+        "brown" : {
+          100 : "#ECE0D1",
+          300 : "#DBC1AC",
+          600 : "#967259",
+          900 : "#634832"
+        }
+      },
+      boxShadow : {
+        "normal" : "0px 1px 10px rgba(0 , 0 , 0, 0.05)"
+      },
+      borderRadius:{
+        "4xl" : "2rem"
+      },
+      fontFamily : {
+        "Dana":"Dana",
+        "DanaMedium":"Dana Medium",
+        "DanaDemiBold":"Dana DemiBold",
+        "MorabbaLight":"Morabba Light",
+        "MorabbaMedium":"Morabba Medium",
+        "MorabbaBold":"Morabba Bold",
+      },
+      letterSpacing : {
+        "tightest" : "-0.065em"
+      },
+      spacing:{
+        '30' : '7.5rem'
+      }
+    },
+  },
+  plugins: [
+    function({addVariant}){
+      addVariant('child' , '& > *');
+      addVariant('child-hover' , '& > *:hover');
+    }
+  ],
+}
+
